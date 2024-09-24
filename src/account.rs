08bc9ba30d5ee18e05671c09be05851b3faf817b
Dan Norris <dan@cosmonic.com>
2024-09-23 20:04:51 -0400

    fix: fix mem_storage option, max_bytes_required in Jetstream limits

--- src/account.rs
+++ src/account.rs
@@ -24,7 +24,7 @@ pub struct OperatorLimits {
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 #[serde(rename_all = "snake_case")]
 pub struct JetStreamLimits {
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "mem_storage", skip_serializing_if = "Option::is_none")]
     pub memory_storage: Option<i64>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub disk_storage: Option<i64>,
@@ -39,7 +39,7 @@ pub struct JetStreamLimits {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub disk_max_stream_bytes: Option<i64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub max_bytes_resp: Option<i64>,
+    pub max_bytes_required: Option<i64>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
